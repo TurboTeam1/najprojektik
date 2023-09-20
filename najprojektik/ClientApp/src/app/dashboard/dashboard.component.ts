d81c1e6a5cb3e9e6dff48d13ef757dac2ab67118
Jakub Chalúpka <jakubchalupka385@gmail.com>
2023-09-20 21:06:46 +0200

    123

--- najprojektik/ClientApp/src/app/dashboard/dashboard.component.ts
+++ najprojektik/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
+interface User {
+  xp: number;
+  guild: string;
+}
+
 export class DashboardComponent {
+
+  user: User = {
+    xp: 100,
+    guild: 'Adventurers'
+  };
+
+
 
 }
